7dcfd48eb90e9d21c370498a78c5314576e5cbce
Adam Havlíček <havlicek.adam23@gmail.com>
2022-07-19 15:44:35 +0200

    chore: update gradle wrapper #0

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,8 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.3.2")
     testImplementation("io.kotest:kotest-assertions-core:5.3.2")
     testImplementation("io.kotest:kotest-property:5.3.2")
-    testImplementation("io.kotest.extensions:kotest-assertions-arrow")
-    testImplementation("io.kotest.extensions:kotest-extensions-spring")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:1.2.5")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.1")
 }
 
 tasks.withType<KotlinCompile> {
